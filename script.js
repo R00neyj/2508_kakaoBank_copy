a5906e6eb2bcc7a7b1235c06551f192d744709b1
R00neyj <yjw1555@gmail.com>
2025-09-29 13:08:51 +0900

    section8 end

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ function scrollAnimation__init() {
   const target = document.querySelectorAll(
     `
     .section3,
-    .section6,
-    .section7
+    .section6
     `
   );
 
@@ -60,9 +59,68 @@ function scrollAnimation__init() {
     observer.observe(el);
   });
 }
+function dayNightAni__init() {
+  const target = document.querySelectorAll(
+    `
+    .section7
+    `
+  );
+
+  target.forEach((el) => {
+    el.classList.add("animate-init");
+  });
+
+  const observer2Init = function (entries) {
+    entries.forEach((entry) => {
+      let isIntersecting = entry.isIntersecting;
+      if (isIntersecting) {
+        entry.target.classList.remove("animate-init");
+        entry.target.classList.add("animate");
+      }
+    });
+  };
+
+  const observer2 = new IntersectionObserver(observer2Init, {
+    root: null,
+    threshold: 0.5,
+  });
+
+  target.forEach((el) => {
+    observer2.observe(el);
+  });
+}
+
+function section8swiper() {
+  const target = document.querySelector(".section8 .card-swiper");
+  const prevBtn = document.querySelector(".section8 .prev-btn");
+  const nextBtn = document.querySelector(".section8 .next-btn");
+
+  const swiper = new Swiper(target, {
+    // Optional parameters
+    // direction: "vertical",
+    loop: true,
+    speed: 400,
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 0,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: nextBtn,
+      prevEl: prevBtn,
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    // },
+  });
+}
 
 // function load list
 document.addEventListener("DOMContentLoaded", function () {
   siteMapModal();
   scrollAnimation__init();
+  dayNightAni__init();
+  section8swiper();
 });
